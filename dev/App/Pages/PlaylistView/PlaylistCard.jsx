76a1894c19d50fe35a72cd854990eba59f91943a
jaimemendozadev <jaimemendozadev@gmail.com>
2018-08-22 13:40:30 -0700

    Add rotate animation to playlist card
    May or may not use in final version.

--- dev/App/Pages/PlaylistView/PlaylistCard.jsx
+++ dev/App/Pages/PlaylistView/PlaylistCard.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 const PlaylistCard = ({playlistInfo}) => (
   <div className="playlistcard">
     {console.log('playlistInfo is ', playlistInfo)}
+    <h3>{playlistInfo.name}</h3>
   </div>
 )
 
