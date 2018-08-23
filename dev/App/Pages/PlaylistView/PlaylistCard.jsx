765fab440ff1180ca023773594b96fe84cea8a14
jaimemendozadev <jaimemendozadev@gmail.com>
2018-08-23 12:05:56 -0700

    Create working animated playlist card
    Playlist card flips over on hover to show
    songs in playlist.

--- dev/App/Pages/PlaylistView/PlaylistCard.jsx
+++ dev/App/Pages/PlaylistView/PlaylistCard.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+const renderSongNames = songs => {
+  return songs.map(song => {
+    return <p key={song.id_user_id_identifier
+    }>{song.title}</p>
+  })
+}
+
+
 const PlaylistCard = ({ playlistInfo }) => (
-  <div className="playlistcard">
-    {console.log("playlistInfo is ", playlistInfo)}
-    <h3>{playlistInfo.name}</h3>
+  <div className="playlist-wrapper">
+    <div className="playlistcard">
+        {console.log("playlistInfo is ", playlistInfo)}
+        <div className="playlistcard-front">
+           <h3>{playlistInfo.name}</h3>
+        </div>
+        
+        <div className="playlistcard-back">
+          {renderSongNames(playlistInfo.songs)}
+        </div>
+    </div>
   </div>
 );
 
