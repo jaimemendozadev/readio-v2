c902984e7ea1d14328d3fe957dbb58fd46370d14
jaimemendozadev <jaimemendozadev@gmail.com>
2018-09-04 08:05:12 -0700

    Change file paths in Apollo index

--- dev/App/Apollo/index.jsx
+++ dev/App/Apollo/index.jsx
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
-import { defaults } from "./API/defaults/index.jsx";
-import { resolvers } from "./API/resolvers/index.jsx";
-import { typeDefs } from "./API/typeDefs/index.jsx";
+import { defaults } from "./API/defaults/index.js";
+import { resolvers } from "./API/resolvers/index.js";
+import { typeDefs } from "./API/typeDefs/index.js";
 
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
