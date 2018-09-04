5b6e72dff8e31009a18d747dceea2cffe16ab0f5
jaimemendozadev <jaimemendozadev@gmail.com>
2018-09-04 08:10:32 -0700

    Remove morgan from package.json

--- server/services/middleware/index.js
+++ server/services/middleware/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const morgan = require("morgan");
 const passport = require("../Passport");
 const applyRouterMiddlware = require("../router");
 const staticAssets = path.resolve(__dirname, "../../../public");
 
 const applyMiddleware = app => {
-  app.use(morgan("tiny"));
   app.use(express.static(staticAssets));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
