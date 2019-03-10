c1c5f7670c3c578c7e4df98d83885f47d3a96885
jaimemendozadev <jaimemendozadev@gmail.com>
2019-03-10 12:56:59 -0700

    Upgrade react dependencies

--- webpack/server.js
+++ webpack/server.js
@@ -2,27 +2,24 @@ const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const entry = path.resolve(__dirname, '../index.js');
 
-
 //need target: 'node' for dotenv and mongoose modules
 //https://github.com/mrsteele/dotenv-webpack
 
 module.exports = {
-  target: 'node', 
-  
+  target: 'node',
+
   entry,
-  
+
   output: {
     filename: 'server.js',
-    path: path.resolve(__dirname, '../dist')
+    path: path.resolve(__dirname, '../dist'),
   },
 
   module: {
     rules: [
-      { test: /\.js?x$/, exclude: /node_modules/, loader: 'babel-loader'},
-      { test: /\.(graphql|gql)$/, exclude: /node_modules/, loader: 'raw-loader'}
-    ]
+      {test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader'},
+      {test: /\.(graphql|gql)$/, exclude: /node_modules/, loader: 'raw-loader'},
+    ],
   },
-  plugins: [
-    new Dotenv()
-  ]
-}
+  plugins: [new Dotenv()],
+};
