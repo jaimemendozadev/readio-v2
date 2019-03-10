b7272972dd3b90b25ad98b995096032bd62c1269
jaimemendozadev <jaimemendozadev@gmail.com>
2019-03-10 13:08:45 -0700

    Remove unused babel dependency

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,6 +1,5 @@
-import "babel-polyfill";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App/index.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/index.jsx';
 
-ReactDOM.render(<App />, document.querySelector(".container"));
+ReactDOM.render(<App />, document.querySelector('.container'));
