b9d2b37d24030f0a416ab281ed1d46871579f1fe
jaimemendozadev <jaimemendozadev@gmail.com>
2019-03-15 22:22:48 -0700

    Refactor Home to functional component

--- dev/App/Pages/Home/index.jsx
+++ dev/App/Pages/Home/index.jsx
@@ -1,47 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {GET_USER_INFO} from '../../Apollo/API/graphql/index.js';
 import PlaylistSection from './PlaylistSection.jsx';
 import CustomQuery from '../../Components/CustomQuery.jsx';
 import {saveFetchedUser} from './utils';
 
-const defaultState = {
-  currentUser: {},
-};
+const Home = () => (
+  <CustomQuery query={GET_USER_INFO}>
+    {(data, client) => {
+      const {getUser} = data;
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = defaultState;
-  }
+      console.log('current user in Home component from getUser query', getUser);
 
-  render() {
-    return (
-      <CustomQuery query={GET_USER_INFO}>
-        {(data, client) => {
-          const {getUser} = data;
+      saveFetchedUser(getUser, client);
 
-          console.log(
-            'current user in Home component from getUser query',
-            getUser,
-          );
+      return (
+        <div>
+          <h1>
+            {getUser
+              ? `Welcome to Read.io ${getUser.first_name}!`
+              : `Read.io - Home Page`}
+          </h1>
 
-          saveFetchedUser(getUser, client);
-
-          return (
-            <div>
-              <h1>
-                {getUser
-                  ? `Welcome to Read.io ${getUser.first_name}!`
-                  : `Read.io - Home Page`}
-              </h1>
-
-              <PlaylistSection playlists={getUser.playlists} />
-            </div>
-          );
-        }}
-      </CustomQuery>
-    );
-  }
-}
+          <PlaylistSection playlists={getUser.playlists} />
+        </div>
+      );
+    }}
+  </CustomQuery>
+);
 
 export default Home;
